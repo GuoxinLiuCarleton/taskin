6425fb695db2bd0b0c1d6edccf76eacb6de1deff
leo <liuguoxin.good@gmail.com>
2022-06-15 18:15:59 -0400

    feature add form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
 
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
 
-var createTaskHandler = function() {
-    var tasksToDoEl = document.querySelector("#tasks-to-do");
-    var listItemEl = document.createElement("li");
-    listItemEl.textContent = "This is a new task.";
-    listItemEl.className = "task-item";
-    tasksToDoEl.appendChild(listItemEl);
-    // alert("button clicked");
-  }
-buttonEl.addEventListener("click", createTaskHandler);
+  formEl.addEventListener("submit", createTaskHandler);
